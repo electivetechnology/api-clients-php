644ed48618f25efc6d3cd51f4d8561100428867f
Kris Rybak <kris.rybak@hotmail.co.uk>
2021-05-17 18:43:46 +0100

    Adds Oauth client

---
+++ src/OAuth/Client.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Elective\ApiClients\OAuth;
+
+use Elective\ApiClients\Result;
+use Elective\ApiClients\ApiClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Elective\ApiClients\OAuth\Client
+ *
+ * @author Kris Rybak <kris.rybak@electivegroup.com>
+ */
+class Client extends ApiClient
+{
+    public const OAUTH_URL              = 'https://oauth.connect.staging.et-ns.net';
+    public const PATH_AUTHORIZATIONS    = '/v1/oauth2/authorizations';
+
+    public function __construct(
+        HttpClientInterface $client,
+        string $oauthBaseUrl = self::OAUTH_URL,
+        bool $isEnabled = true,
+        RequestStack $request
+    ) {
+        $this->setClient($client);
+        $this->setIsEnabled($isEnabled);
+        $this->setBaseUrl($oauthBaseUrl);
+        $token = $request->getCurrentRequest() ? $request->getCurrentRequest()->headers->get('authorization') : false;
+
+        if ($token) {
+            $pos = strpos($token, 'Bearer ');
+            if (!is_null($pos)) {
+                $str = substr($token, 7);
+
+                $this->setToken($str);
+            }
+        }
+    }
+
+    /**
+     * Create new Authorization
+     *
+     * @return Result
+     */
+    public function createAuthorization($clientId, $vendor, $scopes = null, $username = null): ?Result
+    {
+        $payload = new \StdClass();
+        $payload->clientId = $clientId;
+        $payload->vendor = $vendor;
+        $payload->scopes = $scopes;
+        $payload->username = $username;
+
+        // Prepare client options
+        $options = [];
+
+        // Set Token for this request
+        $options['auth_bearer'] = $this->getToken();
+
+        // Set data
+        $options['json'] = $payload;
+
+        // Create request URL
+        $requestUrl = $this->getBaseUrl() . self::PATH_AUTHORIZATIONS;
+
+        // Send request
+        return $this->handleRequest('POST', $requestUrl, $options);
+    }
+}
